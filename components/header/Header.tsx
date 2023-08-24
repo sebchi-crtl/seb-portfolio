63782a540f3bf31322956d3dc8ef5af4a3a65f43
sebastine <chiemelienwobodo8@gmail.com>
2023-08-24 15:37:32 +0100

    added next/image

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,17 +1,18 @@
+import Image from "next/image";
 import {AiFillTwitterCircle} from "react-icons/ai"
 
 const Header = () => {
     return (
         <section className="min-h-screen text-center">
             <div className="text-red-700 px-10">
-                <h1 className="text-5xl py-5 text-teal-700 font-medium ">
+                <h1 className="text-5xl py-3 text-teal-700 font-medium ">
                     Chiemelie Nwobodo
                     {/* text-gray-900 dark:text-gray-100 */}
                 </h1>
-                <h3>
+                <h3 className="text-2xl py-2">
                     Developer and Designer 
                 </h3>
-                <p className="text-gray-500 dark:text-gray-400">
+                <p className="text-gray-500 dark:text-gray-400 text-md leading-6">
                 Lorem ipsum dolor sit amet, 
                 consectetur adipiscing elit, 
                 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
@@ -19,9 +20,12 @@ const Header = () => {
                 nisi ut aliquip ex ea commodo consequat.
                     
                 </p>
-                <div className="soc">
+                {/* <div className="soc">
                 <AiFillTwitterCircle />
-                </div>
+                </div> */}
+            </div>
+            <div className="img">
+                <Image src={} />
             </div>
         </section>
     );
