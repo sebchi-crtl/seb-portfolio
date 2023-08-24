f0d005bd27ff77b3c27beea64d9bae1370a39869
sebastine <chiemelienwobodo8@gmail.com>
2023-08-24 15:31:35 +0100

    centerlised all

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,18 +2,26 @@ import {AiFillTwitterCircle} from "react-icons/ai"
 
 const Header = () => {
     return (
-        <section className="min-h-screen ">
-            <div className="text-red-700">
-                <h1 className="text-4xl font-bold ">
-                    NAME
+        <section className="min-h-screen text-center">
+            <div className="text-red-700 px-10">
+                <h1 className="text-5xl py-5 text-teal-700 font-medium ">
+                    Chiemelie Nwobodo
                     {/* text-gray-900 dark:text-gray-100 */}
                 </h1>
                 <h3>
                     Developer and Designer 
                 </h3>
                 <p className="text-gray-500 dark:text-gray-400">
-                    EMAIL
+                Lorem ipsum dolor sit amet, 
+                consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat.
+                    
                 </p>
+                <div className="soc">
+                <AiFillTwitterCircle />
+                </div>
             </div>
         </section>
     );
