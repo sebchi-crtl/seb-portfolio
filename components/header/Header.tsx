7921058a0033875e8636cec1833dbc453bb1395b
sebastine <chiemelienwobodo8@gmail.com>
2023-09-01 00:20:05 +0100

    chnaged header name  color

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -4,21 +4,20 @@ import devseb from "@/public/image/mypix.png"
 
 const Header = () => {
     return (
-        <section className="min-h-screen my-10 text-center">
+        <section className="min-h-screen my-10 text-center max-w-[40rem]">
             <div className="text-red-700 px-10 font-bold">
-                <h1 className="text-5xl py-3 text-teal-700 ">
+                <h1 className="text-5xl py-3 text-neutral-700 ">
                     Chiemelie Nwobodo
                     {/* text-gray-900 dark:text-gray-100 */}
                 </h1>
                 <h3 className="text-2xl py-2">
-                    Developer and Designer 
+                    Developer and Engineer 
                 </h3>
-                <p className="text-gray-500 dark:text-gray-400 text-md leading-6 font-semibold">
-                Lorem ipsum dolor sit amet, 
-                consectetur adipiscing elit, 
-                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
-                nisi ut aliquip ex ea commodo consequat.
+                <p className="text-gray-500 dark:text-gray-400 text-md leading-6 font-semibold ">
+                Inspired by the adage 'predict the future by inventing it,' 
+                I, a dedicated software engineer, am poised to be a trailblazer. 
+                With a vision for innovation and a drive to create impactful solutions, 
+                I am excited to contribute my skills to startups and established companies, shaping the future.
                     
                 </p>
                 {/* <div className="soc">
